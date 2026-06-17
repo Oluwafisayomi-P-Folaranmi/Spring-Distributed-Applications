c80a1b98f3151b716c9d933c59eea1450ee85646
Oluwafisayomi-P-Folaranmi <folaranmfisayomi@gmail.com>
2026-06-17 15:52:39 +0100

    Yes

---
+++ Core/Resiliency_patterns/suastuscar/src/test/java/com/example/suastuscar/SuastuscarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suastuscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuastuscarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
